41748be9a37022b9462f7293af2a9332388df425
Arnoldodany44 <arnoldovaldez@gmail.com>
2022-11-24 09:44:00 -0700

    Add app.js configuration

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const config = require('config');
+const {expressjwt} = require('express-jwt');
+const i18n = require('i18n');
 
 const indexRouter = require('./routes/index');
 const projectsRouter = require('./routes/projects');
@@ -14,8 +18,24 @@ const storiesRouter = require('./routes/stories');
 const releasesRouter = require('./routes/releases');
 const workersRouter = require('./routes/workers');
 
+const uri= config.get("dbChain");
+//"mongodb://localhost:27017/videoclub";
+mongoose.connect(uri);
+const db = mongoose.connection;
 const app = express();
 
+db.on('open',()=>{
+  console.log("Connection successful");
+});
+db.on('error',()=>{
+  console.log("Error: Could not connect to the database.");
+});
+
+i18n.configure({
+  locales:['es','en'],
+  cookie: 'language',
+  directory: `${__dirname}/locales`
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -25,6 +45,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const jwtKey = config.get("secret.key");
 
 app.use('/', indexRouter);
 app.use('/projects', projectsRouter);
